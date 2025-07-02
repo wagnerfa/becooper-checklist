1bc762fb3c01caaf26e90642517c7ba34c523221
WagnerAndrade <wagnerandrade.dev@gmail.com>
2025-07-02 15:26:09 -0300

    Alteração total projeto

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,24 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     return render_template('index.html')
+
+
+@main.route('/cadastro_salas')
+def cadastro_salas():
+    return render_template('index.html')
+
+
+@main.route('/cadastro_equipamentos')
+def cadastro_equipamentos():
+    return render_template('index.html')
+
+
+@main.route('/formulario')
+def formulario():
+    return render_template('index.html')
+
+
+@main.route('/formulario/cadastro')
+def formulario_cadastro():
+    return render_template('index.html')
+
